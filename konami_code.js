507790b329b5db29976fd6b7a2ea11a287e5ead4
Kawalpreet Kaur <kawalpreet.kaur@hotmail.com>
2018-08-21 18:22:57 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -5,7 +5,7 @@ let index=0;
 function init(event) {
   // Write your JavaScript code inside the init() function
   let bodyTag=document.getElementsByTagName('body');
-  let keypressed=bodyTag.addEventListener(event.which);
+  let keypressed=bodyTag.addEventListener('keydown',event.which);
   console.log(keypressed);
   let convertedValue=parseInt(keypressed);
    if (convertedValue===code[index] && index<10){
