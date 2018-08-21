66a4b3897b8f8d1e9cd53bf2610439d73580dd76
Kawalpreet Kaur <kawalpreet.kaur@hotmail.com>
2018-08-21 18:12:02 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,9 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
-function init() {
+let index=0;
+
+function init(e) {
   // Write your JavaScript code inside the init() function
-let bodyTag=document.getElementBYTAg
+let bodyTag=document.getElementsByTagName('body');
+if ()
 }
